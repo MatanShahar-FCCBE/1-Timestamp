8879ae65e70f4829b97a211c335a9093769403dd
Matan Shahar <mat.matans@gmail.com>
2017-09-17 20:09:23 +0300

    Server now handles incomming requests.
    The server can now answers incomming API request.
    Right now there is not landing page.

---
+++ server/index.ts
@@ -0,0 +1,45 @@
+import * as http from "http";
+import * as moment from "moment";
+
+interface IResponse {
+    unix: number,
+    natural: string
+}
+
+const config = require("./server.json");
+const server = http.createServer((req, res) => {
+    if(req.method != "GET")
+    {
+        res.statusCode = 405;
+        res.end();
+        return;
+    }
+
+    res.statusCode = 400;
+    var response: IResponse = {
+        unix: null,
+        natural: null
+    };
+
+    var reqString = req.url ? req.url.substr(1) : "undefined";
+    var date: moment.Moment;
+    if(/^\d+$/.test(reqString)) {
+        date = moment(reqString, "X");
+    } else {
+        date = moment(reqString, "MMMM D, YYYY");
+    }
+
+    if(date && date.isValid())
+    {
+        res.statusCode = 200;
+        response.unix = date.unix();
+        response.natural = date.format("MMMM D, YYYY");
+    }
+
+    res.setHeader('Content-Type', 'text/json');
+    res.end(JSON.stringify(response));
+});
+
+server.listen(config.port, config.hostname, () => {
+    console.log(`Server running at http://${config.hostname}:${config.port}/`);
+});
---
+++ server/typings.d.ts
@@ -0,0 +1,4 @@
+declare module "config.json" {
+    export let hostname: string;
+    export let port: number;
+}
