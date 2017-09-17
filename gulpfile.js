e0a7e89c09aac209b68ce54c3b870dbdd7e7f83e
Matan Shahar <mat.matans@gmail.com>
2017-09-17 19:26:32 +0300

    Configured build system using gulp.
    Configured a simple build system using gulp, allowing:
    1. Compilation of typescript files.
    2. Multi environment configuration (using json files) using $env.
    
    Additional changes:
    1. Updated .gitignore to ignore the publish directory (dist).
    2. package.json - installed gulp and added start and build scripts.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var typescript = require("gulp-typescript");
+
+var env = gutil.env.env || "dev";
+
+var paths = {
+    buildPath: "obj/build/" + env + "/",
+    commonConfig: "config/*.json",
+    envConfig: "config/" + env + "/*.json",
+    publishPath: "dist/"
+}
+
+function copyToDest(src) {
+    return src.pipe(gulp.dest(paths.buildPath));
+}
+
+gulp.task("json-config", function() {
+    return copyToDest(gulp.src([paths.commonConfig, paths.envConfig]));
+});
+
+gulp.task("json-config-common", function() {
+    return copyToDest(gulp.src(paths.commonConfig));
+});
+
+gulp.task("json-config-env", function() {
+    return copyToDest(gulp.src(paths.envConfig));
+});
+
+gulp.task("tscript", function() {
+    var project = typescript.createProject("tsconfig.json");
+    return gulp.src(project.config.include) 
+        .pipe(project())
+        .pipe(gulp.dest(paths.buildPath));
+});
+
+gulp.task("publish", ["build"], function() {
+    return gulp.src(paths.buildPath + "**/*")
+        .pipe(gulp.dest(paths.publishPath));
+});
+
+gulp.task("build", [ "json-config",  "tscript" ]);
+gulp.task("default", ["build", "publish"]);
